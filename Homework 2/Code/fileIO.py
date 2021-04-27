e86d018017fed039026789215b6f938a62005754
Luke2336 <lukeyu.eecs07@nctu.edu.tw>
2021-04-27 20:57:48 +0800

    finish

--- Homework 2/Code/fileIO.py
+++ Homework 2/Code/fileIO.py
@@ -1,14 +1,16 @@
 def read_input(inputFileName):
-    inputFile = open(inputFileName)
+    inputFile = open(inputFileName, "r")
     lines = inputFile.readlines()
     A = []
     for line in lines:
-        A.append(map(int, line.split()))
+        L = list(map(int, line.split()))
+        if len(L) > 0:
+            A.append(L)
     inputFile.close()
     return A
 
 
 def write_output(outputFileName, ans):
-    outputFile = open(outputFileName)
-    outputFile.write(ans)
+    outputFile = open(outputFileName, "w")
+    outputFile.write(str(ans) + '\n')
     outputFile.close()
